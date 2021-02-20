2a76cc4e34a54bcfa8eebe74b92fc422c8f84ac3
sfali23 <f.syed.ali@gmail.com>
2021-02-19 19:36:38 -0500

    test cases for ManualBumpingVersusAutobumpingSpec

---
+++ src/test/scala/com/alphasystem/sbt/release/ManualBumpingVersusAutobumpingSpec.scala
@@ -0,0 +1,186 @@
+package com.alphasystem.sbt.release
+
+import com.alphasystem.sbt.release.test._
+import io.circe.generic.auto._
+import org.scalatest.Assertion
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.prop.TableDrivenPropertyChecks
+
+class ManualBumpingVersusAutobumpingSpec
+    extends AnyFunSuite
+    with TableDrivenPropertyChecks
+    with Matchers {
+
+  import ManualBumpingVersusAutobumpingSpec._
+
+  private val defaultConfiguration =
+    SemanticBuildVersionConfiguration(
+      snapshot = false,
+      preReleasePrefix = "pre."
+    )
+
+  forAll(
+    DataGenerator
+      .tableFor8(
+        getClass.getSimpleName,
+        classOf[DataSet1].getSimpleName,
+        DataSet1.Headers,
+        (value: DataSet1) => DataSet1.unapply(value).get
+      )
+  ) {
+    (
+      autobumpTag: Option[String],
+      preReleaseTag: Option[String],
+      bump: VersionComponent,
+      newPreRelease: Boolean,
+      promoteToRelease: Boolean,
+      forceBump: Boolean,
+      annotated: Boolean,
+      expectedVersion: String
+    ) =>
+      val autobumpTagMsg = autobumpTag.map("[" + _ + "]").getOrElse("[]")
+      val preReleaseTagMsg = preReleaseTag.map("[" + _ + "]").getOrElse("[]")
+      test(
+        s"""autobumping $autobumpTagMsg version, preReleaseTag $preReleaseTagMsg, manually bumping $bump version, 
+           |manual newPreRelease $newPreRelease, manual promoteToRelease $promoteToRelease, forceBump $forceBump 
+           |expectedVersion: $expectedVersion (annotated: $annotated)"""
+          .stripMargin
+          .replaceNewLines
+      ) {
+        new TestSpec {
+          override protected def populateRepository(): Unit =
+            testRepository
+              .makeChanges()
+              .commitAndTag("1.2.3-pre.4", annotated)
+              .makeChanges()
+              .commit(
+                s"This is a message $autobumpTagMsg $preReleaseTagMsg"
+              )
+
+          override protected def assertion: Assertion = {
+            val config = defaultConfiguration.copy(
+              forceBump = forceBump,
+              newPreRelease = newPreRelease,
+              promoteToRelease = promoteToRelease,
+              componentToBump = bump
+            )
+            SemanticBuildVersion(
+              workingDir,
+              config
+            ).determineVersion shouldBe expectedVersion
+          }
+        }
+      }
+  }
+
+  forAll(
+    DataGenerator
+      .tableFor8(
+        getClass.getSimpleName,
+        classOf[DataSet2].getSimpleName,
+        DataSet2.Headers,
+        (value: DataSet2) => DataSet2.unapply(value).get
+      )
+  ) {
+    (
+      autobumpTag: Option[String],
+      preReleaseTag: Option[String],
+      bump: VersionComponent,
+      newPreRelease: Boolean,
+      promoteToRelease: Boolean,
+      forceBump: Boolean,
+      annotated: Boolean,
+      expectedExceptionMessage: String
+    ) =>
+      val autobumpTagMsg = autobumpTag.map("[" + _ + "]").getOrElse("[]")
+      val preReleaseTagMsg = preReleaseTag.map("[" + _ + "]").getOrElse("[]")
+      test(
+        s"""autobumping $autobumpTagMsg version, preReleaseTag $preReleaseTagMsg, manually bumping $bump version, 
+           |manual newPreRelease $newPreRelease, manual promoteToRelease $promoteToRelease, forceBump $forceBump - 
+           |should fail (annotated: $annotated)""".stripMargin
+      ) {
+        new TestSpec {
+          override protected def populateRepository(): Unit =
+            testRepository
+              .makeChanges()
+              .commitAndTag("1.2.3-pre.4", annotated)
+              .makeChanges()
+              .commit(
+                s"This is a message $autobumpTagMsg $preReleaseTagMsg"
+              )
+
+          override protected def assertion: Assertion = {
+            val config = defaultConfiguration.copy(
+              forceBump = forceBump,
+              newPreRelease = newPreRelease,
+              promoteToRelease = promoteToRelease,
+              componentToBump = bump
+            )
+            val caught =
+              intercept[IllegalArgumentException](
+                SemanticBuildVersion(
+                  workingDir,
+                  config
+                ).determineVersion
+              )
+
+            caught.getMessage shouldBe expectedExceptionMessage
+          }
+        }
+      }
+  }
+}
+
+object ManualBumpingVersusAutobumpingSpec {
+
+  private case class DataSet1(
+    autobumpTag: Option[String] = None,
+    preReleaseTag: Option[String] = None,
+    bump: VersionComponent = VersionComponent.NONE,
+    newPreRelease: Boolean = false,
+    promoteToRelease: Boolean = false,
+    forceBump: Boolean = false,
+    annotated: Boolean = false,
+    expectedVersion: String = "")
+
+  private object DataSet1 {
+
+    val Headers
+      : (String, String, String, String, String, String, String, String) = (
+      "autobumpTag",
+      "preReleaseTag",
+      "bump",
+      "newPreRelease",
+      "promoteToRelease",
+      "forceBump",
+      "annotated",
+      "expectedVersion"
+    )
+  }
+
+  private case class DataSet2(
+    autobumpTag: Option[String] = None,
+    preReleaseTag: Option[String] = None,
+    bump: VersionComponent = VersionComponent.NONE,
+    newPreRelease: Boolean = false,
+    promoteToRelease: Boolean = false,
+    forceBump: Boolean = false,
+    annotated: Boolean = false,
+    expectedExceptionMessage: String = "")
+
+  private object DataSet2 {
+
+    val Headers
+      : (String, String, String, String, String, String, String, String) = (
+      "autobumpTag",
+      "preReleaseTag",
+      "bump",
+      "newPreRelease",
+      "promoteToRelease",
+      "forceBump",
+      "annotated",
+      "expectedExceptionMessage"
+    )
+  }
+}
