02e37696674eb55129b7e9aa3c38adc9ae258e8b
sfali23 <f.syed.ali@gmail.com>
2021-02-22 12:07:02 -0500

    refactor default values

--- src/main/scala/com/alphasystem/sbt/semver/release/internal/SemanticBuildVersionConfiguration.scala
+++ src/main/scala/com/alphasystem/sbt/semver/release/internal/SemanticBuildVersionConfiguration.scala
@@ -1,23 +1,23 @@
 package com.alphasystem.sbt.semver.release.internal
 
-import com.alphasystem.sbt.semver.release.VersionComponent
+import com.alphasystem.sbt.semver.release._
 
 import scala.util.matching.Regex
 
 case class SemanticBuildVersionConfiguration(
-  startingVersion: String = "0.1.0",
-  tagPrefix: String = "v",
-  tagPattern: Regex = "\\d++\\.\\d++\\.\\d++".r,
-  snapshotSuffix: String = "SNAPSHOT",
-  preReleasePrefix: String = "RC.",
-  autoBumpEnable: Boolean = true,
-  forceBump: Boolean = false,
-  promoteToRelease: Boolean = false,
-  snapshot: Boolean = true,
-  newPreRelease: Boolean = false,
+  startingVersion: String = DefaultStartingVersion,
+  tagPrefix: String = DefaultTagPrefix,
+  tagPattern: Regex = DefaultTagPattern,
+  snapshotSuffix: String = DefaultSnapshotSuffix,
+  preReleasePrefix: String = DefaultPreReleasePrefix,
+  autoBumpEnable: Boolean = DefaultAutoBumpEnable,
+  forceBump: Boolean = DefaultForceBump,
+  promoteToRelease: Boolean = DefaultPromoteToRelease,
+  snapshot: Boolean = DefaultSnapshot,
+  newPreRelease: Boolean = DefaultNewPreRelease,
   autoBump: AutoBump = AutoBump(),
   versionsMatching: VersionsMatching = VersionsMatching(),
-  componentToBump: VersionComponent = VersionComponent.NONE) {
+  componentToBump: VersionComponent = DefaultComponentToBump) {
 
   def isAutobumpEnabled: Boolean = autoBumpEnable && autoBump.isEnabled
 
--- src/main/scala/com/alphasystem/sbt/semver/release/package.scala
+++ src/main/scala/com/alphasystem/sbt/semver/release/package.scala
@@ -1,6 +1,20 @@
 package com.alphasystem.sbt.semver
 
+import scala.util.matching.Regex
+
 package object release {
+
+  val DefaultStartingVersion: String = "0.1.0"
+  val DefaultTagPrefix: String = "v"
+  val DefaultTagPattern: Regex = "\\d++\\.\\d++\\.\\d++".r
+  val DefaultSnapshotSuffix: String = "SNAPSHOT"
+  val DefaultPreReleasePrefix: String = "RC."
+  val DefaultAutoBumpEnable: Boolean = true
+  val DefaultForceBump: Boolean = false
+  val DefaultPromoteToRelease: Boolean = false
+  val DefaultSnapshot: Boolean = true
+  val DefaultNewPreRelease: Boolean = false
+  val DefaultComponentToBump: VersionComponent = VersionComponent.NONE
 
   implicit class StringOps(src: String) {
     def replaceNewLines: String = src.replaceAll(System.lineSeparator(), "")
