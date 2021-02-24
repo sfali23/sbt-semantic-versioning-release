b4fd1c8cf9e1956ee34b02a24b3b7e70494d670b
sfali23 <f.syed.ali@gmail.com>
2021-02-24 10:31:02 -0500

    updating description of settings

--- src/main/scala/com/alphasystem/sbt/semver/release/plugin/SemanticVersioningReleasePlugin.scala
+++ src/main/scala/com/alphasystem/sbt/semver/release/plugin/SemanticVersioningReleasePlugin.scala
@@ -15,34 +15,112 @@ object SemanticVersioningReleasePlugin extends AutoPlugin {
 
   object autoImport {
 
-    val determineVersion = taskKey[String]("releaseVersion")
+    val determineVersion = taskKey[String](
+      "A task to determine the next version to be without bumping actual version."
+    )
 
-    val startingVersion = settingKey[String]("startingVersion")
+    val startingVersion = settingKey[String](
+      """This option defines starting version of the build in case there is no tag available to determine next version. 
+        |Default value is "0.1.0-SNAPSHOT".""".stripMargin.replaceNewLines
+    )
 
     val tagPrefix = settingKey[String](
-      "This option defines an optional prefix to use when tagging a release. Default value is \"v\"."
+      "This option defines prefix to use when tagging a release. Default value is \"v\"."
     )
-    val tagPattern = settingKey[Regex]("tagPattern")
-    val snapshotSuffix = settingKey[String]("snapshotSuffix")
-    val preReleasePrefix = settingKey[String]("preReleasePrefix")
-    val autoBumpEnable = settingKey[Boolean]("autoBumpEnable")
-    val forceBump = settingKey[Boolean]("forceBump")
-    val newPreRelease = settingKey[Boolean]("newPreRelease")
-    val promoteToRelease = settingKey[Boolean]("promoteToRelease")
-    val snapshot = settingKey[Boolean]("snapshot")
-    val componentToBump = settingKey[Option[String]]("componentToBump")
-    val autoBumpMajorPattern = settingKey[Option[Regex]]("autoBumpMajorPattern")
-    val autoBumpMinorPattern = settingKey[Option[Regex]]("autoBumpMinorPattern")
-    val autoBumpPatchPattern = settingKey[Option[Regex]]("autoBumpPatchPattern")
+
+    val tagPattern = settingKey[Regex](
+      "This option defines the pattern to identify tag. Default value is \"\\d++\\.\\d++\\.\\d++\"."
+    )
+
+    val snapshotSuffix = settingKey[String](
+      "This option defines the suffix for snapshot. Default value is \"SNAPSHOT\"."
+    )
+
+    val preReleasePrefix = settingKey[String](
+      "This option defines the prefix for the preRelease. Default value is \"RC.\"."
+    )
+
+    val autoBumpEnable = settingKey[Boolean](
+      "This option defines the flag to enable autoBump. Default value is \"true\"."
+    )
+
+    val forceBump = settingKey[Boolean](
+      s"""This option defines the flag to enable forceBump. Default value is "false". This option can be set
+         | via system property "sbt.release.forceBump"."""
+        .stripMargin
+        .replaceNewLines
+    )
+
+    val newPreRelease = settingKey[Boolean](
+      s"""This option defines the flag to enable new-pre-release. Default value is "false". This option can be set
+         | via system property "sbt.release.newPreRelease" as well as via "\\[new-pre-release]" regular expression."""
+        .stripMargin
+        .replaceNewLines
+    )
+
+    val promoteToRelease = settingKey[Boolean](
+      s"""This option defines the flag to enable promote-to-release. Default value is "false". This option can be set
+         | via system property "sbt.release.newPreRelease" as well as via "\\[promote]" regular expression."""
+        .stripMargin
+        .replaceNewLines
+    )
+
+    val snapshot = settingKey[Boolean](
+      s"""This option defines the flag to make current release a snapshot release. Default value is "true". This option
+         | can be set via system property "sbt.release.snapshot"."""
+        .stripMargin
+        .replaceNewLines
+    )
+
+    val componentToBump = settingKey[Option[String]](
+      s"""This option defines the component to bump. Default value is "NONE", which corresponds to bumping 
+         |the lowest precedence component. Acceptable values are "PRE_RELEASE", "PATCH", "MINOR", "MAJOR", going from 
+         |lowest precedence to highest precedence."""
+        .stripMargin
+        .replaceNewLines
+    )
+
+    val autoBumpMajorPattern = settingKey[Option[Regex]](
+      s"""This option defines the regular expression to bump "MAJOR" component. Default value is "\\[major]"."""
+    )
+
+    val autoBumpMinorPattern = settingKey[Option[Regex]](
+      s"""This option defines the regular expression to bump "MINOR" component. Default value is "\\[minor]"."""
+    )
+
+    val autoBumpPatchPattern = settingKey[Option[Regex]](
+      s"""This option defines the regular expression to bump "PATCH" component. Default value is "\\[patch]"."""
+    )
 
     val autoBumpNewPreReleasePattern =
-      settingKey[Option[Regex]]("autoBumpNewPreReleasePattern")
+      settingKey[Option[Regex]](
+        s"""This option defines the regular expression to bump "PRE_RELEASE" component. Default value is
+           | "\\[new-pre-release]".""".stripMargin.replaceNewLines
+      )
 
     val autoBumpPromoteToReleasePattern =
-      settingKey[Option[Regex]]("autoBumpPromoteToReleasePattern")
-    val majorVersionsMatching = settingKey[Int]("majorVersionsMatching")
-    val minorVersionsMatching = settingKey[Int]("minorVersionsMatching")
-    val patchVersionsMatching = settingKey[Int]("patchVersionsMatching")
+      settingKey[Option[Regex]](
+        s"""This option defines the regular expression to bump a . Default value is "\\[promote]"."""
+      )
+
+    val majorVersionsMatching = settingKey[Int](
+      s"""This option defines filtering of a particular major version. Default value is "-1", which is 
+         |corresponds to no matching.""".stripMargin.replaceNewLines
+    )
+
+    val minorVersionsMatching = settingKey[Int](
+      s"""This option defines filtering of a particular minor version. Default value is "-1", which is 
+         |corresponds to no matching."""
+        .stripMargin
+        .replaceNewLines
+    )
+
+    val patchVersionsMatching = settingKey[Int](
+      s"""This option defines filtering of a particular patch version. Default value is "-1", which is 
+         |corresponds to no matching."""
+        .stripMargin
+        .replaceNewLines
+    )
 
     object ReleaseKeys {
       val versionToBe = AttributeKey[String]("Next version")
