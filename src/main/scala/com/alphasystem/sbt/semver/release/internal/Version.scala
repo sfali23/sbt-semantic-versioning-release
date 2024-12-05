9e3a3e56573f4d5e19558f8984e40786bdb1796e
sfali23 <f.syed.ali@gmail.com>
2024-12-05 17:58:21 -0500

    added snapshot component

--- src/main/scala/com/alphasystem/sbt/semver/release/internal/Version.scala
+++ src/main/scala/com/alphasystem/sbt/semver/release/internal/Version.scala
@@ -14,54 +14,83 @@ case class Version(
   patch: Int,
   hotfix: Option[Int] = None,
   preRelease: Option[PreReleaseVersion] = None,
-  snapshot: Option[Boolean] = None,
+  snapshot: Option[Snapshot] = None,
   preReleaseConfig: PreReleaseConfig = PreReleaseConfig()) {
 
-  def bumpMajor: Version = {
+  import VersionComponent.*
+
+  def isHotfix: Boolean = hotfix.isDefined
+
+  def isPreRelease: Boolean = preRelease.isDefined
+
+  def isSnapshot: Boolean = snapshot.isDefined
+
+  private def bumpMajor: Version = {
     if (preRelease.nonEmpty) {
       throw new IllegalArgumentException("Current version is a a pre-release.")
     }
     copy(major = major + 1, minor = 0, patch = 0, hotfix = None, preRelease = None)
   }
 
-  def bumpMinor: Version = {
+  private def bumpMinor: Version = {
     if (preRelease.nonEmpty) {
       throw new IllegalArgumentException("Current version is a a pre-release.")
     }
     copy(minor = minor + 1, patch = 0)
   }
 
-  def bumpPatch: Version = {
+  private def bumpPatch: Version = {
     if (preRelease.nonEmpty) {
       throw new IllegalArgumentException("Current version is a a pre-release.")
     }
     copy(patch = patch + 1)
   }
 
-  def bumpHotfix: Version = {
+  private def bumpHotfix: Version = {
     if (preRelease.nonEmpty) {
       throw new IllegalArgumentException("Current version is a a pre-release.")
     }
     copy(hotfix = hotfix.map(_ + 1).orElse(Some(1)))
   }
 
-  def bumpPreRelease: Version = {
+  private def bumpPreRelease: Version = {
     if (preRelease.isEmpty) {
       throw new IllegalArgumentException("Current version is not a pre-release.")
     }
     copy(preRelease = preRelease.map(pr => pr.copy(version = pr.version + 1)))
   }
 
-  def newPreRelease(componentToBump: VersionComponent): Version = {
+  private def newPreRelease: Version = {
     if (preRelease.isDefined) {
       throw new IllegalArgumentException("Current version is already pre-release")
     }
-    val maybePreReleaseVersion = preReleaseConfig.toInitialPreReleaseVersion
-    componentToBump match {
-      case VersionComponent.MAJOR => bumpMajor.copy(preRelease = maybePreReleaseVersion)
-      case VersionComponent.MINOR => bumpMinor.copy(preRelease = maybePreReleaseVersion)
-      case _                      => bumpPatch.copy(preRelease = maybePreReleaseVersion)
+    copy(preRelease = preReleaseConfig.toInitialPreReleaseVersion)
+  }
+
+  private def promoteToRelease: Version = copy(preRelease = None)
+
+  private def bumpSnapshot(snapshot: Option[Snapshot]): Version = copy(snapshot = snapshot)
+
+  def bumpVersion(snapshot: Option[Snapshot], componentsToBump: VersionComponent*): Version =
+    componentsToBump.foldLeft(this) { case (result, versionComponent) =>
+      versionComponent match {
+        case NONE               => result
+        case MAJOR              => result.bumpMajor
+        case MINOR              => result.bumpMinor
+        case PATCH              => result.bumpPatch
+        case HOT_FIX            => result.bumpHotfix
+        case NEW_PRE_RELEASE    => result.newPreRelease
+        case PRE_RELEASE        => result.bumpPreRelease
+        case PROMOTE_TO_RELEASE => result.promoteToRelease
+        case SNAPSHOT           => result.bumpSnapshot(snapshot)
+      }
     }
+
+  def toStringValue(tagPrefix: String = DefaultTagPrefix): String = {
+    val hotfixValue = hotfix.map(v => s".$v").getOrElse("")
+    val preReleaseValue = preRelease.map(_.toStringValue).getOrElse("")
+    val snapshotValue = snapshot.map(_.toStringValue).getOrElse("")
+    s"$tagPrefix$major.$minor.$patch$hotfixValue$preReleaseValue$snapshotValue"
   }
 }
 
@@ -80,7 +109,7 @@ object Version {
     patch: Int,
     hotfix: Option[Int] = None,
     preRelease: Option[PreReleaseVersion] = None,
-    snapshot: Option[Boolean] = None,
+    snapshot: Option[Snapshot] = None,
     preReleaseConfig: PreReleaseConfig = PreReleaseConfig()
   ): Version = new Version(
     major = major,
@@ -92,7 +121,7 @@ object Version {
     preReleaseConfig = preReleaseConfig
   )
 
-  def apply(version: String, preReleaseConfig: PreReleaseConfig): Version = {
+  def apply(version: String, snapshotSuffix: String, preReleaseConfig: PreReleaseConfig): Version = {
     val matcher = VersionRegex.findAllIn(version)
     // Group 0 will be the entire version string, Groups 1 - 3 will major, minor, and patch version, so there must be
     // at least four groups. There will total of 6 groups
@@ -101,14 +130,18 @@ object Version {
     }
     // pre-release and snapshot
     val maybePreReleaseOrSnapshot = Option(matcher.group(4))
+    val metaInfo = Option(matcher.group(5))
 
     val maybePreReleaseVersion =
-      maybePreReleaseOrSnapshot match {
+      maybePreReleaseOrSnapshot.map(_.replaceAll(s"-$snapshotSuffix", "")) match {
         case Some(value) => preReleaseConfig.toPreReleaseVersion(value)
         case None        => None
       }
 
-    // group 5 contains something line "+build.123", which we are not supporting
+    val maybeSnapshot = maybePreReleaseOrSnapshot
+      .map(_.replaceAll(preReleaseConfig.preReleasePartPattern, ""))
+      .filter(_.contains(snapshotSuffix))
+      .map(_ => Snapshot(snapshotSuffix, metaInfo))
 
     Version(
       major = matcher.group(1).toInt,
@@ -116,15 +149,17 @@ object Version {
       patch = matcher.group(3).toInt,
       hotfix = Option(matcher.group(6)).map(_.toInt),
       preRelease = maybePreReleaseVersion,
-      snapshot = maybePreReleaseOrSnapshot.filter(_.contains("SNAPSHOT")).map(_.toBoolean),
+      snapshot = maybeSnapshot,
       preReleaseConfig = preReleaseConfig
     )
   }
 
+  // This is descending ordering
   implicit val VersionOrdering: Ordering[Version] = (v1: Version, v2: Version) => {
-    val hotFixValues = (v1.hotfix.getOrElse(0), v2.hotfix.getOrElse(0))
-    val preReleaseValues = (v1.preRelease.map(_.version).getOrElse(0), v2.preRelease.map(_.version).getOrElse(0))
-    compareTo(List((v1.major, v2.major), (v1.minor, v2.minor), (v1.patch, v2.patch), hotFixValues, preReleaseValues))
+    val hotFixValues = (v2.hotfix.getOrElse(0), v1.hotfix.getOrElse(0))
+    val preReleaseValues =
+      (v2.preRelease.map(_.version).getOrElse(Int.MaxValue), v1.preRelease.map(_.version).getOrElse(Int.MaxValue))
+    compareTo(List((v2.major, v1.major), (v2.minor, v1.minor), (v2.patch, v1.patch), hotFixValues, preReleaseValues))
   }
 
   @tailrec
@@ -139,9 +174,17 @@ object Version {
     }
 }
 
-case class PreReleaseVersion(prefix: String, version: Int, suffix: Option[String] = None)
+case class PreReleaseVersion(prefix: String, version: Int, suffix: Option[String] = None) {
+
+  def toStringValue: String = s"-$prefix$version${suffix.getOrElse("")}"
+}
 
 object PreReleaseVersion {
   implicit val PreReleaseVersionOrdering: Ordering[PreReleaseVersion] = (x: PreReleaseVersion, y: PreReleaseVersion) =>
-    x.version.compareTo(y.version)
+    y.version.compareTo(x.version)
+}
+
+case class Snapshot(suffix: String, meta: Option[String] = None) {
+
+  def toStringValue: String = s"-$suffix${meta.map(value => s"+$value").getOrElse("")}"
 }
--- src/test/scala/com/alphasystem/sbt/semver/release/internal/VersionSpec.scala
+++ src/test/scala/com/alphasystem/sbt/semver/release/internal/VersionSpec.scala
@@ -10,44 +10,87 @@ import sbtsemverrelease.PreReleaseConfig
 
 class VersionSpec extends AnyWordSpec with Matchers {
 
-  private val preReleaseConfig = PreReleaseConfig(preReleasePartPattern = "(RC)(.)([1-9]\\d*)")
+  import VersionComponent.*
+
+  private val preReleaseConfig = PreReleaseConfig()
 
   "Version parser" should {
 
     "parse simple version" in {
-      Version("0.12.345", PreReleaseConfig()) shouldBe Version(0, 12, 345)
+      Version("0.12.345", DefaultSnapshotSuffix, PreReleaseConfig()) shouldBe Version(0, 12, 345)
     }
 
     "parse hot fix version" in {
-      Version("1.89.22.1", PreReleaseConfig()) shouldBe Version(1, 89, 22, Some(1))
+      Version("1.89.22.1", DefaultSnapshotSuffix, PreReleaseConfig()) shouldBe Version(1, 89, 22, Some(1))
     }
 
     "parse pre-release version" in {
-      Version("1.89.22-RC.5", preReleaseConfig) shouldBe Version(
-        1,
-        89,
-        22,
-        preRelease = Some(PreReleaseVersion("RC.", 5)),
-        preReleaseConfig = preReleaseConfig
-      )
+      Version("1.89.22-RC.5", DefaultSnapshotSuffix, preReleaseConfig) shouldBe
+        Version(
+          1,
+          89,
+          22,
+          preRelease = Some(PreReleaseVersion("RC.", 5)),
+          preReleaseConfig = preReleaseConfig
+        )
+    }
+
+    "parse snapshot version without meta info" in {
+      Version("2.123.7-SNAPSHOT", DefaultSnapshotSuffix, preReleaseConfig) shouldBe
+        Version(2, 123, 7, snapshot = Some(Snapshot(DefaultSnapshotSuffix)), preReleaseConfig = preReleaseConfig)
+    }
+
+    "parse snapshot version wit meta info" in {
+      Version("0.45.799-SNAPSHOT+abcde", DefaultSnapshotSuffix, preReleaseConfig) shouldBe
+        Version(
+          0,
+          45,
+          799,
+          snapshot = Some(Snapshot(DefaultSnapshotSuffix, Some("abcde"))),
+          preReleaseConfig = preReleaseConfig
+        )
+    }
+
+    "parse version containing both pre-release followed by snapshot" in {
+      Version("6.0.13-RC.3-SNAPSHOT+abcd", DefaultSnapshotSuffix, preReleaseConfig) shouldBe
+        Version(
+          6,
+          0,
+          13,
+          None,
+          Some(PreReleaseVersion("RC.", 3)),
+          Some(Snapshot(DefaultSnapshotSuffix, Some("abcd"))),
+          preReleaseConfig
+        )
     }
   }
 
   "Version sort" should {
     "compare simple version" in {
-      Seq(
-        Version("0.12.346", preReleaseConfig),
-        Version("0.12.345-RC.2", preReleaseConfig),
-        Version("1.89.22-RC.5", preReleaseConfig),
-        Version("0.12.345", preReleaseConfig),
-        Version("0.12.345-RC.1", preReleaseConfig)
-      ).sorted shouldBe Seq(
-        Version("0.12.345", preReleaseConfig),
-        Version("0.12.345-RC.1", preReleaseConfig),
-        Version("0.12.345-RC.2", preReleaseConfig),
-        Version("0.12.346", preReleaseConfig),
-        Version("1.89.22-RC.5", preReleaseConfig)
-      )
+      val actual = Seq(
+        Version("0.12.346", DefaultSnapshotSuffix, preReleaseConfig),
+        Version("0.12.345-RC.2", DefaultSnapshotSuffix, preReleaseConfig),
+        Version("0.12.346.1", DefaultSnapshotSuffix, preReleaseConfig),
+        Version("1.89.22-RC.5", DefaultSnapshotSuffix, preReleaseConfig),
+        Version("0.12.345", DefaultSnapshotSuffix, preReleaseConfig),
+        Version("0.12.345-RC.1", DefaultSnapshotSuffix, preReleaseConfig)
+      ).sorted.map(_.toStringValue())
+      val expected = Seq("v1.89.22-RC.5", "v0.12.346.1", "v0.12.346", "v0.12.345", "v0.12.345-RC.2", "v0.12.345-RC.1")
+      actual shouldBe expected
+    }
+
+    "sort versions" in {
+      val actual = Seq(
+        Version("0.1.0", DefaultSnapshotSuffix, preReleaseConfig),
+        Version("0.2.0", DefaultSnapshotSuffix, preReleaseConfig),
+        Version("0.2.1", DefaultSnapshotSuffix, preReleaseConfig),
+        Version("1.0.0", DefaultSnapshotSuffix, preReleaseConfig),
+        Version("1.1.0", DefaultSnapshotSuffix, preReleaseConfig),
+        Version("1.1.0-RC.1", DefaultSnapshotSuffix, preReleaseConfig),
+        Version("1.1.0-RC.2", DefaultSnapshotSuffix, preReleaseConfig)
+      ).sorted.map(_.toStringValue())
+      val expected = Seq("v1.1.0", "v1.1.0-RC.2", "v1.1.0-RC.1", "v1.0.0", "v0.2.1", "v0.2.0", "v0.1.0")
+      actual shouldBe expected
     }
   }
 
@@ -55,27 +98,27 @@ class VersionSpec extends AnyWordSpec with Matchers {
 
     "bump patch version" in {
       val expectedVersion = Version(major = 0, minor = 1, patch = 0, preReleaseConfig = preReleaseConfig)
-      expectedVersion.bumpPatch shouldBe expectedVersion.copy(patch = 1)
+      expectedVersion.bumpVersion(None, PATCH) shouldBe expectedVersion.copy(patch = 1)
     }
 
     "bump minor version" in {
       val expectedVersion = Version(major = 0, minor = 1, patch = 3, preReleaseConfig = preReleaseConfig)
-      expectedVersion.bumpMinor shouldBe expectedVersion.copy(minor = 2, patch = 0)
+      expectedVersion.bumpVersion(None, MINOR) shouldBe expectedVersion.copy(minor = 2, patch = 0)
     }
 
     "bump major version" in {
       val expectedVersion = Version(major = 0, minor = 5, patch = 23, preReleaseConfig = preReleaseConfig)
-      expectedVersion.bumpMajor shouldBe expectedVersion.copy(major = 1, minor = 0, patch = 0)
+      expectedVersion.bumpVersion(None, MAJOR) shouldBe expectedVersion.copy(major = 1, minor = 0, patch = 0)
     }
 
     "bump hotfix version" in {
       val expectedVersion = Version(major = 1, minor = 2, patch = 7, preReleaseConfig = preReleaseConfig)
-      expectedVersion.bumpHotfix shouldBe expectedVersion.copy(hotfix = Some(1))
+      expectedVersion.bumpVersion(None, HOT_FIX) shouldBe expectedVersion.copy(hotfix = Some(1))
     }
 
     "create new pre-release version with patch version bump" in {
       val expectedVersion = Version(major = 2, minor = 0, patch = 17, preReleaseConfig = preReleaseConfig)
-      expectedVersion.newPreRelease(VersionComponent.PATCH) shouldBe expectedVersion.copy(
+      expectedVersion.bumpVersion(None, PATCH, NEW_PRE_RELEASE) shouldBe expectedVersion.copy(
         patch = 18,
         preRelease = Some(PreReleaseVersion("RC.", 1))
       )
@@ -83,7 +126,7 @@ class VersionSpec extends AnyWordSpec with Matchers {
 
     "create new pre-release version with minor version bump" in {
       val expectedVersion = Version(major = 2, minor = 0, patch = 17, preReleaseConfig = preReleaseConfig)
-      expectedVersion.newPreRelease(VersionComponent.MINOR) shouldBe expectedVersion.copy(
+      expectedVersion.bumpVersion(None, MINOR, NEW_PRE_RELEASE) shouldBe expectedVersion.copy(
         minor = 1,
         patch = 0,
         preRelease = Some(PreReleaseVersion("RC.", 1))
@@ -92,7 +135,7 @@ class VersionSpec extends AnyWordSpec with Matchers {
 
     "create new pre-release version with major version bump" in {
       val expectedVersion = Version(major = 2, minor = 0, patch = 17, preReleaseConfig = preReleaseConfig)
-      expectedVersion.newPreRelease(VersionComponent.MAJOR) shouldBe expectedVersion.copy(
+      expectedVersion.bumpVersion(None, MAJOR, NEW_PRE_RELEASE) shouldBe expectedVersion.copy(
         major = 3,
         minor = 0,
         patch = 0,
@@ -108,8 +151,15 @@ class VersionSpec extends AnyWordSpec with Matchers {
         preRelease = Some(PreReleaseVersion("RC.", 1)),
         preReleaseConfig = preReleaseConfig
       )
-      expectedVersion.bumpPreRelease shouldBe expectedVersion.copy(preRelease = Some(PreReleaseVersion("RC.", 2)))
+      expectedVersion.bumpVersion(None, PRE_RELEASE) shouldBe
+        expectedVersion.copy(preRelease = Some(PreReleaseVersion("RC.", 2)))
     }
 
+    "create snapshot version with minor version bump" in {
+      val expectedVersion = Version(major = 1, minor = 3, patch = 5, preReleaseConfig = preReleaseConfig)
+      val snapshot = Some(Snapshot("SNAPSHOT", Some("asdqwe")))
+      expectedVersion.bumpVersion(snapshot, SNAPSHOT, MINOR) shouldBe
+        expectedVersion.copy(minor = 4, patch = 0, snapshot = snapshot)
+    }
   }
 }
