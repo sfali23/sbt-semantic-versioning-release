dfc48d64e07bcc41a066102cc23959c6afcd8bb0
sfali23 <f.syed.ali@gmail.com>
2024-12-05 17:57:50 -0500

    updated enums for version component

--- src/main/java/com/alphasystem/sbt/semver/release/VersionComponent.java
+++ src/main/java/com/alphasystem/sbt/semver/release/VersionComponent.java
@@ -1,7 +1,28 @@
 package com.alphasystem.sbt.semver.release;
 
+import java.util.Map;
+
 public enum VersionComponent {
-    NONE(-1), PRE_RELEASE(3), PATCH(2), MINOR(1), MAJOR(0);
+    NONE(0),
+    MAJOR(2),
+    MINOR(4),
+    PATCH(8),
+    HOT_FIX(16),
+    NEW_PRE_RELEASE(32),
+    PRE_RELEASE(64),
+    PROMOTE_TO_RELEASE(128),
+    SNAPSHOT(256);
+
+    private static final Map<Integer, VersionComponent> indexToComponent = Map.of(
+            NONE.index, VersionComponent.NONE,
+            MAJOR.index, VersionComponent.MAJOR,
+            MINOR.index, VersionComponent.MINOR,
+            PATCH.index, VersionComponent.PATCH,
+            HOT_FIX.index, VersionComponent.HOT_FIX,
+            NEW_PRE_RELEASE.index, VersionComponent.NEW_PRE_RELEASE,
+            PRE_RELEASE.index, VersionComponent.PRE_RELEASE,
+            PROMOTE_TO_RELEASE.index, VersionComponent.PROMOTE_TO_RELEASE,
+            SNAPSHOT.index, VersionComponent.SNAPSHOT);
 
     private final int index;
 
@@ -31,5 +52,9 @@ public enum VersionComponent {
 
     public boolean isNone() {
         return index == -1;
+    }
+
+    public static VersionComponent fromIndex(int index) {
+        return indexToComponent.get(index);
     }
 }
