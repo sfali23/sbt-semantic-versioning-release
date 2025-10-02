4f358ad0e51ee4b3af587af3104f51412a738026
sfali <f.syed.ali@gmail.com>
2025-10-02 07:39:59 -0400

    update dependencies

--- project/Settings.scala
+++ project/Settings.scala
@@ -5,13 +5,13 @@ object Settings {
 
   object V {
     val CucumberExpressions = "18.0.1"
-    val CucumberJunit = "7.20.1"
-    val CucumberScala = "8.25.1"
-    val EclipseJGit = "7.1.0.202411261347-r"
-    val LogbackClassic = "1.5.12"
+    val CucumberJunit = "7.30.0"
+    val CucumberScala = "8.33.0"
+    val EclipseJGit = "7.3.0.202506031305-r"
+    val LogbackClassic = "1.5.19"
     val ScalaTest = "3.3.0-SNAP4"
-    val Slf4jApi = "2.0.16"
-    val TypesafeConfig = "1.4.3"
+    val Slf4jApi = "2.0.17"
+    val TypesafeConfig = "1.4.5"
   }
 
   val Dependencies: Seq[ModuleID] = Seq(
